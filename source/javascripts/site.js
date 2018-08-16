03b0afb11530ebdbb32f84d35446dcd6998cfd5c
Stephan <stephan-pavlovic@gmx.de>
2018-08-16 13:45:34 +0200

    add mobile menu

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,8 @@
-// This is where it all goes :)
+// without jQuery (doesn't work in older IEs)
+document.addEventListener('DOMContentLoaded', function(){
+  console.log(document.getElementById("hamburger"), 'HALLO')
+  document.getElementById("hamburger").addEventListener('click', function(e){
+    console.log(document.getElementById("menu"), 'HALLO')
+    document.getElementById("menu").classList.toggle('asOpen')
+  }, false)
+}, false);
